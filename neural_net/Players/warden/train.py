8648fcbebf0cbfb2e1ed18719b9c4b0253ce9127
Iridum-png <baker-e16@boswells-school.com>
2022-12-27 21:58:31 +0000

    Changed neural net - still broken

--- neural_net/Players/warden/train.py
+++ neural_net/Players/warden/train.py
@@ -1,7 +1,10 @@
 from warden_engine import WardenEngine
 
-if __name__ == '__main__':
+def main():
     warden = WardenEngine()
     warden.load(r"C:\Users\ed9ba\Documents\Coding\NEA\Warden\neural_net\Players\warden\warden_weights.h5")
     warden.train()
     warden.save(r"C:\Users\ed9ba\Documents\Coding\NEA\Warden\neural_net\Players\warden\warden_weights.h5")
+
+if __name__ == '__main__':
+    main()
--- neural_net/Players/warden/warden_engine.py
+++ neural_net/Players/warden/warden_engine.py
@@ -7,7 +7,7 @@ class WardenEngine:
     def __init__(self):
         # Define the CNN architecture
         self.model = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(128, (2, 2), activation='relu', input_shape=(8, 8, 6)),
+            tf.keras.layers.Conv2D(128, (2, 2), activation='relu', input_shape=(8, 8, 13)),
             tf.keras.layers.MaxPooling2D((2, 2)),
             tf.keras.layers.Conv2D(128, (2, 2), activation='relu'),
             tf.keras.layers.MaxPooling2D((2, 2)),
@@ -18,8 +18,10 @@ class WardenEngine:
 
         # Compile the model
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        
+        self.called = False
 
-    def train(self, num_games=10000):
+    def train(self, num_games=10000, temperature=0.5):
         # Generate training data
         X = []
         Y = []
@@ -29,14 +31,24 @@ class WardenEngine:
             board = chess.Board()
             while not board.is_game_over():
                 input_board = self.get_input_array(board.fen())
-                prediction = self.model.predict(input_board.reshape(1, 8, 8, 6))
+                prediction = self.model.predict(input_board.reshape(1, 8, 8, 13))
+
+                # Add some randomness to the move selection process by sampling from a categorical distribution
+                # with the predicted move probabilities as the weights
+                move_probs = prediction[0]
                 if board.turn:
                     # White's turn
-                    move = chess.Move.from_uci(board.legal_moves[prediction > 0.5][0])
+                    move_probs = np.exp(move_probs / temperature)
+                    move_probs /= sum(move_probs)
+                    move_index = np.random.choice(len(move_probs), p=move_probs)
+                    move = chess.Move.from_uci(board.legal_moves[move_index])
                     board.push(move)
                 else:
                     # Black's turn
-                    move = chess.Move.from_uci(board.legal_moves[prediction < 0.5][0])
+                    move_probs = np.exp(-move_probs / temperature)
+                    move_probs /= sum(move_probs)
+                    move_index = np.random.choice(len(move_probs), p=move_probs)
+                    move = chess.Move.from_uci(board.legal_moves[move_index])
                     board.push(move)
 
             # Add the final board position to the training data
@@ -61,6 +73,12 @@ class WardenEngine:
         # Parse the board part of the FEN string
         rows = parts[0].split('/')
         board = np.zeros((8, 8, 6), dtype=np.int8)
+        
+        if not self.called:
+            self.called = True
+            print(parts)
+            print(rows)
+        
         for i, row in enumerate(rows):
             col = 0
             for c in row:
@@ -74,21 +92,21 @@ class WardenEngine:
                     col += 1
 
         # Add the other parts of the FEN string to the input array
-        input_array = np.zeros((1, 8, 8, 6), dtype=np.int8)
-        # input_array[0, :, :, :6] = board
-        # input_array[0, :, :, 6] = 1 if parts[1] == 'w' else 0
-        # input_array[0, :, :, 7] = 1 if 'K' in parts[2] else 0
-        # input_array[0, :, :, 8] = 1 if 'Q' in parts[2] else 0
-        # input_array[0, :, :, 9] = 1 if 'k' in parts[2] else 0
-        # input_array[0, :, :, 10] = 1 if 'q' in parts[2] else 0
-        # input_array[0, :, :, 11] = int(parts[3]) if parts[3] != '-' else 0
-        # input_array[0, :, :, 12] = int(parts[4]) if parts[4] != '-' else 0
+        input_array = np.zeros((8, 8, 13), dtype=np.int8)
+        input_array[:, :, :6] = board
+        input_array[:, :, 6] = 1 if parts[1] == 'w' else 0
+        input_array[:, :, 7] = 1 if 'K' in parts[2] else 0
+        input_array[:, :, 8] = 1 if 'Q' in parts[2] else 0
+        input_array[:, :, 9] = 1 if 'k' in parts[2] else 0
+        input_array[:, :, 10] = 1 if 'q' in parts[2] else 0
+        input_array[:, :, 11] = int(parts[3]) if parts[3] != '-' else 0
+        input_array[:, :, 12] = int(parts[4]) if parts[4] != '-' else 0
 
         return input_array
 
     def get_move(self, board: chess.Board) -> chess.Move:
         # Predict the next move based on the current board position
-        input_board = np.array(board.fen().split()[0]).reshape((8, 8, 6))
+        input_board = np.array(board.fen().split()[0]).reshape((8, 8, 12))
         prediction = self.model.predict(input_board)
         if prediction > 0.5:
             move = chess.Move.from_uci(board.legal_moves[prediction > 0.5][0])
@@ -103,3 +121,11 @@ class WardenEngine:
     def load(self, filename):
         # Load the model weights from a file
         self.model.load_weights(filename)
+
+def main():
+    warden = WardenEngine()
+    # warden.load(r"C:\Users\ed9ba\Documents\Coding\NEA\Warden\neural_net\Players\warden\warden_weights.h5")
+    warden.train()
+    warden.save(r"C:\Users\ed9ba\Documents\Coding\NEA\Warden\neural_net\Players\warden\warden_weights.h5")
+
+main()
