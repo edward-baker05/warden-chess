bae9cc4e9084bb5305222170751018e0162bee03
Iridum-png <baker-e16@boswells-school.com>
2023-01-03 18:17:08 +0000

    Added ability to get the current FEN position

--- neural_net/Players/human.py
+++ neural_net/Players/human.py
@@ -8,6 +8,10 @@ class Human:
     def get_move(self, board: chess.Board) -> chess.Move:
         """Get the move from the human player"""
         move = input("Enter your move in startend format (eg a1h8): ")
+        if move.lower() == "fen":
+            print(board.fen())
+            move = self.get_move(board)
+            return move
         try:
             move = chess.Move.from_uci(move)
         except chess.InvalidMoveError:
