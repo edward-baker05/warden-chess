9a4a6849d9bfbb46632994deb588e3ffd36f5cb5
Iridum-png <63092175+Iridum-png@users.noreply.github.com>
2023-01-04 10:43:54 +0000

    Added docstrings and typehints

--- neural_net/Players/mtcs_engine/mtcs_engine.py
+++ neural_net/Players/mtcs_engine/mtcs_engine.py
@@ -1,14 +1,24 @@
+from __future__ import annotations
 import math
+from typing import Optional
 import chess
 import tensorflow as tf
 import numpy as np
 
 class TranspositionTable:
-    def __init__(self):
-        # Initialize an empty dictionary to store the transposition table
+    def __init__(self) -> None:
+        """Initialize an empty dictionary to store the transposition table."""
         self.table = {}
 
-    def get(self, board):
+    def get(self, board: chess.Board) -> Optional[float]:
+        """Look up the board position in the transposition table and return the value if it exists.
+
+        Parameters:
+        board: a chess.Board object representing the current board position.
+
+        Returns:
+        The value associated with the board position in the transposition table, or None if the board position is not in the table.
+        """
         # Hash the board position to use as a key in the transposition table
         key = board.fen()
 
@@ -19,7 +29,13 @@ class TranspositionTable:
         # Return None if the key is not in the table
         return None
 
-    def put(self, board, value):
+    def put(self, board: chess.Board, value: float) -> None:
+        """Store the value in the transposition table with the board position as the key.
+
+        Parameters:
+        board: a chess.Board object representing the current board position.
+        value: the value to store in the transposition table.
+        """
         # Hash the board position to use as a key in the transposition table
         key = board.fen()
 
@@ -27,33 +43,60 @@ class TranspositionTable:
         self.table[key] = value
 
 class Node:
-    def __init__(self, board, parent=None):
+    def __init__(self, board: chess.Board, parent: Optional[Node]=None) -> None:
+        """Initialize a node in the Monte Carlo tree.
+
+        Parameters:
+        board: a chess.Board object representing the current board position.
+        parent: the parent Node object. If not provided, defaults to None.
+        """
         self.board = board
         self.parent = parent
         self.children = []
-        self.visits = 0
-        self.wins = 0
+        self.visits = 0.0
+        self.wins = 0.0
         self.player = board.turn
-        self.value = 0
+        self.value = 0.0
         if parent is not None:
             self.depth = parent.depth + 1
         else:
             self.depth = 0
 
-    def add_child(self, move):
+    def add_child(self, move: chess.Move) -> Node:
+        """Add a child node to the current node by making a move on the board.
+
+        Parameters:
+        move: a chess.Move object representing the move to make on the board.
+
+        Returns:
+        The newly created child Node object.
+        """
         new_board = self.board.copy()
         new_board.push(move)
         child = Node(new_board, parent=self)
         self.children.append(child)
         return child
 
-    def ucb1(self):
+   def ucb1(self) -> float:
+        """Calculate and return the UCB1 score for the current node.
+
+        Returns:
+        The UCB1 score for the current node.
+        """
         if self.visits == 0:
             return float('inf')
         return self.value + math.sqrt(2 * math.log(self.parent.visits) / self.visits)
 
 class MonteCarloEngine:
-    def __init__(self, colour, temperature=1, iterations=10000, max_depth=25):
+    def __init__(self, colour: int, temperature: float=1, iterations: int=10000, max_depth: int=25) -> None:
+        """Initialize the Monte Carlo engine.
+
+        Parameters:
+        colour: the chess.WHITE or chess.BLACK value representing the side the engine is playing.
+        temperature: a float value representing the temperature parameter for the softmax function.
+        iterations: the number of iterations to run the Monte Carlo tree search.
+        max_depth: the maximum depth to search in the tree.
+        """
         import os
         # self.model = tf.keras.Sequential([
         #     tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(8, 8, 12)),
@@ -80,14 +123,31 @@ class MonteCarloEngine:
         # Create a transposition table
         self.transposition_table = TranspositionTable()
 
-    def select_leaf(self, node):
+    def select_leaf(self, node: Node) -> Node:
+        """Traverse the tree from the given node to a leaf node by selecting child nodes using the UCB1 algorithm.
+
+        Parameters:
+        node: the Node object to start the selection from.
+
+        Returns:
+        The leaf Node object that was selected.
+        """
         while len(node.children) > 0:
             node = max(node.children, key=lambda c: c.ucb1())
             if node.depth == self.max_depth:
                 return node
         return node
 
-    def search(self, node, model):
+    def search(self, node: Node, model: tf.keras.Model) -> Node:
+        """Perform a Monte Carlo tree search from the given node.
+
+        Parameters:
+        node: the root Node object to start the search from.
+        model: the TensorFlow model to use for evaluating positions.
+
+        Returns:
+        The selected child Node object to continue the search from.
+        """
         for _ in range(self.iterations):
             leaf_node = self.select_leaf(node)
             value = self.evaluate(leaf_node, model)
@@ -105,19 +165,25 @@ class MonteCarloEngine:
         max_index = np.random.choice(range(len(probs)), p=probs)
         return node.children[max_index]
 
-    def evaluate(self, node, model):
+    def evaluate(self, node: Node, model: tf.keras.Model) -> float:
+        """Evaluate the given node using the TensorFlow model.
+
+        Parameters:
+        node: the Node object to evaluate.
+        model: the TensorFlow model to use for evaluating positions.
+
+        Returns:
+        The evaluation of the node as a float value.
+        """
         # Check if the position is in the transposition table
         value = self.transposition_table.get(node.board)
         if value is not None:
             return value
 
-        tensor = board_to_tensor(node.board)
-        prediction = model.predict(tensor, verbose=0)[0][0]
-        print(prediction)
-        value = prediction
-        if node.board.turn == self.colour:
-            value = value
-        else:
+        tensor = board_to_tensor(node.board).reshape(1, 8, 8, 12)
+        value = float(model.predict(tensor, verbose=0)[0][0])
+        print(type(value))
+        if node.board.turn != self.colour:
             value = 1 - value
 
         # Store the value in the transposition table
@@ -125,7 +191,13 @@ class MonteCarloEngine:
 
         return value
 
-    def backpropagate(self, node, value):
+    def backpropagate(self, node: Node, value: float) -> None:
+        """Backpropagate the evaluation value through the tree to update the visit and win counts for each node.
+
+        Parameters:
+        node: the leaf Node object to start the backpropagation from.
+        value: the evaluation value to backpropagate.
+        """
         while node is not None:
             node.visits += 1
             node.wins += value
@@ -133,7 +205,16 @@ class MonteCarloEngine:
             value = 1 - value
             node = node.parent
 
-    def get_move(self, board: chess.Board):
+    def get_move(self, board: chess.Board, model: tf.keras.Model) -> chess.Move:
+        """Get the best move for the given board position according to the Monte Carlo tree search.
+
+        Parameters:
+        board: a chess.Board object representing the current board position.
+        model: the TensorFlow model to use for evaluating positions.
+
+        Returns:
+        A chess.Move object representing the best move for the given board position.
+        """
         # Create a root node for the MCTS tree
         root_node = Node(board)
 
@@ -147,7 +228,15 @@ class MonteCarloEngine:
         print(f"AI made move: {move}")
         return move
 
-def board_to_tensor(board):
+def board_to_tensor(board: chess.Board) -> np.ndarray:
+    """Convert the given board position to a tensor.
+
+    Parameters:
+    board: a chess.Board object representing the current board position.
+
+    Returns:
+    A numpy array representing the board position as a tensor.
+    """
     # Convert the board to a tensor
     tensor = np.zeros((8, 8, 12))
     for i in range(8):
@@ -158,9 +247,14 @@ def board_to_tensor(board):
                     tensor[i][j][piece.piece_type-1] = 1
                 else:
                     tensor[i][j][piece.piece_type + 5] = 1
-    return tensor.reshape(1, 8, 8, 12)
+    return tensor
 
-def create_model():
+def create_model() -> tf.keras.Model:
+    """Create and return a TensorFlow model for evaluating chess positions.
+
+    Returns:
+    A TensorFlow model.
+    """
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu', input_shape=(8, 8, 12)))
     model.add(tf.keras.layers.BatchNormalization())
@@ -194,7 +288,10 @@ def create_model():
     
     return model
 
-def train():
+def train() -> None:
+    """
+    Train the TensorFlow model using the data in the `sample_fen.csv` file. The model is saved to the file `weights.h5` after training.
+    """
     import numpy as np
     import tensorflow as tf
     import pandas as pd
@@ -219,7 +316,7 @@ def train():
 
             for game in games:
                 board = chess.Board(game[0])
-                tensor = board_to_tensor(board=board).reshape(8, 8, 12)  # Convert the board position to an input tensor
+                tensor = board_to_tensor(board=board)  # Convert the board position to an input tensor
                 outcome = game[1]  # win, loss, or draw
                 if outcome == "w":
                     labels.append([0, 1])
@@ -240,14 +337,17 @@ def train():
             model.fit(inputs, labels, epochs=100, batch_size=32)
             print(f"Finished training cycle {i}")
     except KeyboardInterrupt:
-        pass
+        return
 
     # Save the new model weights
     model.save_weights(r"neural_net\Players\mtcs_engine\weights.h5")
     print()
     print("Saved weights to disk")
 
-def get_weights():
+def get_weights() -> None:
+    """
+    Get the weights of the TensorFlow model. The model is created and the weights are loaded from the file `weights.h5`. The weights are then printed to the console.
+    """
     model = create_model()
     model.load_weights(r"neural_net\Players\mtcs_engine\weights.h5")
     for layer in model.layers:
