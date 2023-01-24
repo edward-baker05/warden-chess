bd5bb635bebbf8cc15bbad1556159952f818c704
Iridum-png <baker-e16@boswells-school.com>
2023-01-24 16:23:35 +0000

    Written basic boilerplate

--- cpp_engine/src/main.cpp
+++ cpp_engine/src/main.cpp
@@ -1,8 +1,25 @@
 // Entry point of the program
 
-#include "stdio.h"
+#include <iostream>
+#include "board.h"
+#include "movegen.h"
+#include "search.h"
+#include "eval.h"
+#include "uci.h"
 
-int main(int argc, char** argv)
+int main()
 {
-    printf("Hello World!");
+    Board board;
+    Movegen movegen;
+    Search search;
+    Eval eval;
+    Uci uci;
+
+    // Initialize the chess engine
+    uci.init(board, movegen, search, eval);
+
+    // Start the UCI loop to communicate with a chess GUI
+    uci.loop();
+
+    return 0;
 }
