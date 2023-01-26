1ba5e2683390721933e30a8e2b15a20c64805010
Iridum-png <baker-e16@boswells-school.com>
2023-01-26 19:28:43 +0000

    idk

--- Games/open.py
+++ Games/open.py
@@ -1,21 +1,19 @@
-import chess.pgn
 import chess
+import chess.pgn
+import chess.engine
 import csv
 import os
 
-def display_board():
-    for i, row in enumerate(board.unicode(invert_color=True).split("\n")):
-        print(f"{8-i} {row}")
-    print("  a b c d e f g h")
-    print()
+# Load the Stockfish engine
+engine = chess.engine.SimpleEngine.popen_uci(r"C:/Users/ed9ba/Documents/Coding/NEA/Warden/Games/stockfish-windows-2022-x86-64-avx2.exe")
 
-directory = os.fsencode(r"C:\Users\ed9ba\Documents\Coding\NEA\Warden\Games")
+directory = os.fsencode(r"C:/Users/ed9ba/Documents/Coding/NEA/Warden/Games")
 
-with open(r"..\neural_net\Players\mtcs_engine\validation_fen.csv", "w", newline='') as f:
-    for collection in reversed(os.listdir(directory)):
+with open(r"training_data.csv", "w", newline='') as f:
+    for collection in os.listdir(directory):
         try:
             filename = os.fsdecode(collection)
-            if '2019' not in filename:
+            if not filename.endswith(".pgn"): 
                 continue
             pgn = open(filename)
 
@@ -33,21 +31,22 @@ with open(r"..\neural_net\Players\mtcs_engine\validation_fen.csv", "w", newline=
                     board.push(move)
                 games.append(board)
 
-                # Get the winner from the pgn string
-                winner = 'w' if current_game.headers["Result"] == "1-0" else 'b' if current_game.headers["Result"] == "0-1" else 'd'
-
                 # Write the fen and winner to a csv file
                 try:
                     for i in range(100):
-                        board.pop()
                         fen = board.fen()
                         if fen == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1":
                             break
-                        writer.writerow([fen, winner])
+                        board.pop()
+                        # Get the score from stockfish
+                        score = engine.analyse(board, chess.engine.Limit(time=0.1))['score'].white().score()
+                        writer.writerow([fen, score])
                 except IndexError:
                     pass
                 i += 1
         except UnicodeDecodeError:
             continue
         print(f"File {filename} finished")
+    # Close the engine
+    engine.quit()
 print("Finished")
