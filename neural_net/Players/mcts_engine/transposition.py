3afda29b292b2ea4df29ba921c3d990f23f4d4a4
Iridum-png <baker-e16@boswells-school.com>
2023-03-28 19:56:31 +0100

    Zobrist hashing implemented

--- neural_net/Players/mcts_engine/transposition.py
+++ neural_net/Players/mcts_engine/transposition.py
@@ -1,3 +1,4 @@
+import random
 from typing import Optional
 
 import chess
@@ -6,7 +7,12 @@ import chess
 class TranspositionTable:
     def __init__(self) -> None:
         """Initialize an empty dictionary to store the transposition table."""
-        self.table = {}
+        self.__table = {}
+        self.__zobrist_table = [[random.randint(1, 2**64 - 1) for _ in range(12)] for _ in range(64)]
+        self.__piece_values = {
+            'P': 0, 'N': 1, 'B': 2, 'R': 3, 'Q': 4, 'K': 5,
+            'p': 6, 'n': 7, 'b': 8, 'r': 9, 'q': 10, 'k': 11
+        }
 
     def get(self, board: chess.Board) -> Optional[float]:
         """
@@ -19,11 +25,11 @@ class TranspositionTable:
             The value associated with the board position in the transposition table, or None if the board position is not in the table.
         """
         # Hash the board position to use as a key in the transposition table
-        key = board.fen()
+        key = self.__zobrist_hash(board)
 
         # Look up the key in the table and return the value if it exists
-        if key in self.table:
-            return self.table[key]
+        if key in self.__table:
+            return self.__table[key]
 
         # Return None if the key is not in the table
         return None
@@ -35,4 +41,21 @@ class TranspositionTable:
             board: a chess.Board object representing the current board position.
             value: the value to store in the transposition table.
         """
-        self.table[board.fen()] = value
+        self.__table[self.__zobrist_hash(board)] = value
+
+    def __zobrist_hash(self, board: chess.Board) -> int:        
+        """
+        Calculate the Zobrist hash of the board position.
+        Args:
+            board: a chess.Board object representing the current board position.
+        Returns:
+            The Zobrist hash of the board position as an integer.
+        """
+        h = 0
+        for i in range(64):
+            piece = board.piece_at(i)
+            if piece is not None:
+                j = self.__piece_values[piece.symbol()]
+                h = h ^ self.__zobrist_table[i][j]
+        
+        return h
